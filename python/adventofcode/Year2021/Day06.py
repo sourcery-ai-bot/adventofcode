bb84067cf5745202cda9f95366a34cdc341e1801
Jan Laermann <laermannjan@gmail.com>
2021-12-19 19:14:52 +0100

    day 6 haskell

--- python/adventofcode/Year2021/Day06.py
+++ python/adventofcode/Year2021/Day06.py
@@ -44,19 +44,25 @@ How many lanternfish would there be after 256 days?
 """
 
 
+def step(tracker, delay, offset):
+    tracker = {key - 1: value for key, value in tracker.items()}
+    new_offspring = tracker.pop(-1)
+    tracker[delay - 1] += new_offspring
+    tracker[delay - 1 + offset] = new_offspring
+    return tracker
+
+
 def count_fish_after_days(timings: list[int], days: int):
     delay = 7
     offset = 2
-    tracker = {t: 0 for t in range(delay + offset)}
 
+    tracker = {t: 0 for t in range(delay + offset)}
     for t in timings:
         tracker[int(t)] += 1
 
     for day in range(days):
-        tracker = {key - 1: value for key, value in tracker.items()}
-        new_offspring = tracker.pop(-1)
-        tracker[delay - 1] += new_offspring
-        tracker[delay - 1 + offset] = new_offspring
+        tracker = step(tracker, delay, offset)
+
     return sum(tracker.values())
 
 
